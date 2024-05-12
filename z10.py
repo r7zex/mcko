97ee370317b70233defb235d56ab08f1fc5ff6de
r7zex <151392460+r7zex@users.noreply.github.com>
2024-05-12 21:57:02 +0300

    z10.py

--- z10.py
+++ z10.py
@@ -1,4 +1,4 @@
 inp = input('Введите выражение: ')
 digits = int(input('Введите количество разрядов: '))
-answer = int(bin(eval(inp))[2:], 2)
+answer = int(bin(eval(inp))[2:][-digits:], 2)
 print(answer)
