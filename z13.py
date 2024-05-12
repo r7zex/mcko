3d034a45d482a20cb4d755226109fa1cc1b78ffd
r7zex <151392460+r7zex@users.noreply.github.com>
2024-05-12 19:13:19 +0300

    z13.py

--- z13.py
+++ z13.py
@@ -1,4 +1,4 @@
-file = open(r'E:\Downloads\13.txt')
+file = open(r'Путь к файлу')
 min_sum = 20001
 count = 0
 file_list = []
@@ -6,7 +6,7 @@ for i in file:
     i = i.strip('\n')
     file_list.append(int(i))
 for i in range(len(file_list)-1):
-    if file_list[i] % 8 == 0 and file_list[i+1] % 8 == 0:
+    if 'Условие':
         count += 1
         if file_list[i] + file_list[i+1] < min_sum:
             min_sum = file_list[i] + file_list[i+1]   #Минимальная сумма
